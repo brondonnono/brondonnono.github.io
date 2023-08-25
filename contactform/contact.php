80060da594ae6e33ac53bf9edcb93f4021d41b94
brondonnono <brondonnono3@gmail.com>
2023-08-25 09:54:59 +0100

    trying to get form fields values

--- contactform/contact.php
+++ contactform/contact.php
@@ -1,3 +1,8 @@
 <?php
-    if(isset($_POST['']))
+    if(isset($_POST['sendMail'])) {
+        $name = $_POST['name'];
+        $mail = $_POST['mail'];
+        $subject = $_POST['subject'];
+        $message = $_POST['message'];
+    }
 ?>
