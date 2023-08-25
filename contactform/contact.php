426027a79c2336ae5ffaeea650a0fb291d9aa330
brondonnono <brondonnono3@gmail.com>
2023-08-25 09:55:37 +0100

    updating email field name's  in php file

--- contactform/contact.php
+++ contactform/contact.php
@@ -1,7 +1,7 @@
 <?php
     if(isset($_POST['sendMail'])) {
         $name = $_POST['name'];
-        $mail = $_POST['mail'];
+        $mail = $_POST['email'];
         $subject = $_POST['subject'];
         $message = $_POST['message'];
     }
